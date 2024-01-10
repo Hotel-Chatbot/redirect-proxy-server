f99b40d4560393eb8bb40604ec830f9b7f682472
Dhatchinamoorthi2803 <dhatchinamoorthi.r@codingmart.com>
2024-01-10 15:42:20 +0530

    feat: add server for redirection

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import fastify from "fastify";
 const server = fastify();
-server.get("")
-async function main(){
-    try {
-        await server.listen(3000,"0.0.0.0");
-        console.log("server is running on port 3000")
-       
-        
-    } catch (error) {
-        console.log(error);
+server.get("/:url", (req, res) => {
+  res.redirect(req.params.url);
+});
+async function main() {
+  try {
+    server.listen({ port: 3000, host: "0.0.0.0" }, (err, addr) => {
+      if (err) {
+        console.err(err);
+        server.log.error(err);
         process.exit(1);
-        
-    }
+      }
+    });
+    console.log("server is running on port 3000");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 main();
